516d21ce88bc0f1e19a44113731948086f944074
Kambiz <kambiz.fakhr@yahoo.com>
2023-12-20 00:18:08 +0330

    #all Controller added by API Generator

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() 
+        {
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Activity;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
